22ca09249a95d912157dd61e60373583370fc7c9
José Eliomar <jose.eliomar2244@gmail.com>
2024-06-08 12:55:44 -0300

    Foi criado o microsserviço endereco-ms

---
+++ endereco-ms/src/main/java/com/example/EnderecoMsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnderecoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnderecoMsApplication.class, args);
+	}
+
+}
