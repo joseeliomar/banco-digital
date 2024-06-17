bdedbbdfaff4f73fa5500f810a1ff93fa6a364c8
José Eliomar <jose.eliomar2244@gmail.com>
2024-06-17 18:36:49 -0300

    Criado o microsserviço deposito-ms

---
+++ deposito-ms/src/main/java/com/example/DepositoMsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositoMsApplication.class, args);
+	}
+
+}
