b205873f9c88257f24ff07aed0076b4c01334ef0
José Eliomar <jose.eliomar2244@gmail.com>
2024-07-10 17:01:14 -0300

    Criado o microsserviço transferencia-ms (esse microsserviço tem o objetivo fazer as transferências bancárias).

---
+++ transferencia-ms/src/main/java/com/example/demo/TransferenciaMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciaMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferenciaMsApplication.class, args);
+	}
+
+}
